de806a9a1c7328c72bc4aab122398b9e25683858
encode42 <me@encode42.dev>
2026-02-02 03:25:04 -0500

    Update NetworkOptimizer, specify wwwroot

--- packages/network-optimizer/default.nix
+++ packages/network-optimizer/default.nix
@@ -6,15 +6,18 @@
   fetchFromGitHub,
 }:
 
+let
+  webRootPath = "NetworkOptimizer.Web/wwwroot";
+in
 buildDotnetModule rec {
   pname = "network-optimizer";
-  version = "1.0.12";
+  version = "1.1.9";
 
   src = fetchFromGitHub {
     owner = "Ozark-Connect";
     repo = "NetworkOptimizer";
     tag = "v${version}";
-    hash = "sha256-x3zU4tl5qxe6llzQuvKBcVkh3HLUlQIyFtlK6Me8vLQ=";
+    hash = "sha256-G13GjR5R+f80sCjxKx/3uZmEbURoN69xrM5DFc/SilY=";
   };
 
   nugetDeps = ./deps.json;
@@ -22,11 +25,19 @@ buildDotnetModule rec {
   dotnet-sdk = dotnetCorePackages.sdk_10_0;
   dotnet-runtime = dotnetCorePackages.aspnetcore_10_0;
 
+  projectFile = "src/NetworkOptimizer.Web";
+
   enableParallelBuilding = false;
 
   nativeBuildInputs = with pkgs; [
     git
   ];
+
+  postBuild = ''
+    mkdir -p $out/share/network-optimizer/${webRootPath}
+
+    cp -r src/${webRootPath} $out/share/network-optimizer/${webRootPath}
+  '';
 
   meta = {
     description = "Self-hosted performance optimization and security audit tool for UniFi Networks";
